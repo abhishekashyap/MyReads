6bea25c682b25ca5523ba0eff6b5963aaa6a4b25
abhishekashyap <29458374+abhishekashyap@users.noreply.github.com>
2020-04-24 00:45:02 +0530

    Added url based booklist rendering

--- src/components/BooksList/BooksList.js
+++ src/components/BooksList/BooksList.js
@@ -1,22 +1,30 @@
-import React, { Component } from "react";
-// import { useLocation } from "react-router-dom";
+import React from "react";
+import { useLocation } from "react-router-dom";
 import Book from "../Book/Book";
 
-class BooksList extends Component {
-  render() {
-    // console.log(useLocation());
-    return (
-      <div className="bookshelf-books">
-        <ol className="books-grid">
-          {this.props.myBooks.map((book) => (
-            <li key={book.id}>
-              <Book book={book} addToBookshelf={this.props.addToBookshelf} />
-            </li>
-          ))}
-        </ol>
-      </div>
-    );
-  }
+function BooksList(props) {
+  let url = useLocation();
+  return url.pathname === "/search" ? (
+    <div className="bookshelf-books">
+      <ol className="books-grid">
+        {props.searchedBooks.map((book) => (
+          <li key={book.id}>
+            <Book book={book} addToBookshelf={props.addToBookshelf} />
+          </li>
+        ))}
+      </ol>
+    </div>
+  ) : (
+    <div className="bookshelf-books">
+      <ol className="books-grid">
+        {props.myBooks.map((book) => (
+          <li key={book.id}>
+            <Book book={book} addToBookshelf={props.addToBookshelf} />
+          </li>
+        ))}
+      </ol>
+    </div>
+  );
 }
 
 export default BooksList;
